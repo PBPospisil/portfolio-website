f9f9938085d6f06217f65b2809292c781edd860d
Pat Pospisil <patpospisil@Pats-MacBook-Pro.local>
2023-09-06 17:25:31 -0700

    add second project updated title

--- components/FavouriteProjects.js
+++ components/FavouriteProjects.js
@@ -65,7 +65,7 @@ export default function FavouriteProjects() {
                 className="transform hover:scale-125 transition duration-2000 ease-out"
               />
               <h1 className="absolute top-10 left-10 text-gray-50 font-bold text-xl bg-red-500 rounded-md px-2">
-                PlaceholderTech
+                PDF Reader
               </h1>
               <h1 className="absolute bottom-10 left-10 text-black-50 font-bold text-xl">
                 02
