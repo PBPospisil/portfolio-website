96c4fc5e790fda3e2ae0998d381b3b1639681b57
Pat Pospisil <patpospisil@Pats-MacBook-Pro.local>
2023-09-27 14:32:34 -0700

    constants file splitting

---
+++ constants/smptData.js
@@ -0,0 +1,13 @@
+const smptData = {
+    transporter: {
+        responseIndicators: {
+            success: '250 2.0.0 OK',
+        },
+        configuration: {
+            port: 465,
+            host: 'smtp.gmail.com'
+        }
+    }
+}
+
+export default smptData
